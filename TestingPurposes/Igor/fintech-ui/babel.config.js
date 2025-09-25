c54d2be2295a766ac5f2255e3390424dfa58bb62
Igor Tacu <igor.tacu@isa.utm.md>
2025-09-25 13:33:38 +0300

    brought back the home page + fixed the issues for mobile deployment testing

--- TestingPurposes/Igor/fintech-ui/babel.config.js
+++ TestingPurposes/Igor/fintech-ui/babel.config.js
@@ -1,11 +1,7 @@
-// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // Reanimated plugin MUST be listed last
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
